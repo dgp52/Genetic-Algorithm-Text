fc5e4edb340afb7ca5b8305ceeb0b916957c3132
dgp52 <deeppatel19940@gmail.com>
2017-11-18 17:10:32 -0500

    added methods for testing population and mutation

--- GeneticAlgorithmText/src/GeneticAlgorithm/Main.java
+++ GeneticAlgorithmText/src/GeneticAlgorithm/Main.java
@@ -1,28 +1,55 @@
 package GeneticAlgorithm;
 
 public class Main {
-
+	private String targetString = "test";
+	private int populationSize = 500;
+	private double mutationRate = 0.0;
+	
 	public static void main(String[] args) {
-		String targetString = "test";
-		int populationSize = 100;
-		double mutationRate = 0.02;
 		Main main = new Main();
-		main.geneticAlgorithm(populationSize, mutationRate, targetString);	
+		//main.testPopulationBehaviour();
+		//main.testMutationBehaviour();
+		main.geneticAlgorithm(main.populationSize, main.mutationRate, main.targetString);	
 	}
 	
-	public void geneticAlgorithm(int populationSize, double mutationRate, String targetString) {
+	private void geneticAlgorithm(int populationSize, double mutationRate, String targetString) {
 		Population population = new Population(populationSize, mutationRate, targetString);
-		population.printPopulationDNA();
+		//population.printPopulationDNA();
 		while(true) {
 			if(population.checkPopulation()) {
 				break;
 			} else {
 				population.calculateFitnessValues();
 				population.naturalSelection();
-				population.printPopulationDNA();
+				//population.printPopulationDNA();
 			}
 		}
-		System.out.println(population.getGeneration());
+		System.out.println("Population size: " + populationSize + 
+				", Mutation Rate: " + String.format( "%.2f", mutationRate) +
+				", Generation: " + population.getGeneration());
+	}
+	
+	/*Testing Purposes
+	What happens if we increase the size of the population? 
+	Double the size of population in each loop and compare the number of generations
+	Conclusion: As the number of population increases, the generation decreases*/
+	private void testPopulationBehaviour () {
+		while(populationSize < 10000) {
+			geneticAlgorithm(populationSize, mutationRate, targetString);
+			populationSize = populationSize*2;
+		}
+	}
+	
+	/*What happens if we increase the mutation rate?
+	 Increase mutation by 0.1 in each for loop.
+	 Conclusion: In general, higher mutation rate takes longer generation,but we should also add 
+	 a small change/mutation in every generation. Adding a small amount of mutation is 
+	 important because every generation will be different and unique from previous generation.*/
+	private void testMutationBehaviour () {
+		while(mutationRate <= 1.0) {
+			geneticAlgorithm(populationSize, mutationRate, targetString);
+			mutationRate = mutationRate + 0.01;
+		}
 	}
 
 }
